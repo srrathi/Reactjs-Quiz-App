e27b98eacbc2ef814cabfffdaa39507b81729286
Sitaram Rathi <57089237+srrathi@users.noreply.github.com>
2021-02-01 14:35:28 +0530

    Add files via upload

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { QuizContext } from '../Helpers/Contexts';
 import { Questions } from '../Helpers/QuestionBank';
+import Timer from './Timer';
 
 export const Quiz = () => {
     const [currQues, setCurrQues] = useState(0);
@@ -32,7 +33,9 @@ export const Quiz = () => {
     // }
     return (
         <div className="item-row">
+            <Timer></Timer>
             <h2 className="ques">{Questions[currQues].prompt}</h2>
+            
             <div className="options">
                 <button className="btn btn-sm btn-info" onClick={() => setOptionChosen("A")}>
                     {Questions[currQues].optionA}
@@ -48,10 +51,19 @@ export const Quiz = () => {
                 </button>
             </div>
             {currQues === Questions.length - 1 ? (
-                <button className="btn btn-info btn-finish" onClick={()=> {finishQuiz();}}>Finish Quiz</button>
+                <button className="btn btn-info btn-finish" onClick={() => { finishQuiz(); }}>Finish Quiz</button>
             ) : (
                     <button className="btn btn-info pb" onClick={nextQuestion}>Next Question</button>
                 )}
+            <br /><br />
+            <div className="panel row">
+                <h3 className="ques col-sm-12 col-xs-12">Questions Panel</h3>
+                <div className="btn-div">
+                    {Questions.map(option => {
+                        return (<button className="btn-warning" onClick={() => setCurrQues(Questions.indexOf(option))}>Question {Questions.indexOf(option) + 1}</button>)
+                    })}
+                </div>
+            </div>
 
         </div>
     )
